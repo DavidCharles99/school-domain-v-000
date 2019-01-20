25627426b2bde5c80d8d73c01b1b923fd52a4b7b
David Charles <ratlover03@gmail.com>
2019-01-20 23:38:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,14 @@
 class School
+  attr_reader :school_name, :roster
+  def initialze(school_name)
+    @school_name = school_name
+    @roster = {}
+  end 
+  def add_student(name, age)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
 
+  
+    
 end
